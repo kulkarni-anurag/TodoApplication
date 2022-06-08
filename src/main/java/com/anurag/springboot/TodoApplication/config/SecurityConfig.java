1f6afc2c86272d247e7856940666f370e9cf051f
Anurag Kulkarni <kanurag0801@gmail.com>
2022-06-08 22:16:49 +0530

    Added username & password

---
+++ src/main/java/com/anurag/springboot/TodoApplication/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.anurag.springboot.TodoApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService user(){
+        UserDetails user = User.builder()
+            .username("kulkarni-anurag")
+            .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll()
+            .antMatchers("/", "/*todo**/*").access("hasRole('ADMIN')").and()
+            .formLogin();
+
+        return http.build();
+    }
+}
