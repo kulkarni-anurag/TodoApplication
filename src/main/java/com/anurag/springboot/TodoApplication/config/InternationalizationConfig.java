0fec7839d372dcf1720ec55e5fb2797aa213ddaa
Anurag Kulkarni <kanurag0801@gmail.com>
2022-06-08 23:22:11 +0530

    Added internationalization

---
+++ src/main/java/com/anurag/springboot/TodoApplication/config/InternationalizationConfig.java
@@ -0,0 +1,44 @@
+package com.anurag.springboot.TodoApplication.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class InternationalizationConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("classpath:messages");
+        source.setDefaultEncoding("UTF-8");
+        return source;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("language");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
--- src/main/webapp/WEB-INF/views/welcome.jsp
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,9 +15,9 @@
 <body>
     <%@ include file="common/navbar.jspf" %>
     <div class="container">
-        <h2 class="text-center mt-5">Welcome to Todo Application</h2>
+        <h2 class="text-center mt-5"><spring:message code="welcome.heading" /></h2>
         <br />
-        <h5>Hey there! ${username}</h5>
+        <h5><spring:message code="welcome.greeting" />! ${username}</h5>
         <br />
         <a href="/list-todos">Manage your todos</a>
     </div>
